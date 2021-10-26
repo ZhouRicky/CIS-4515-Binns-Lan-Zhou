ff669b6daf9454bee199d46238907c3937549b8d
RyanLan0213 <lannenghao@gmail.com>
2021-10-25 23:43:59 -0400

    *Added the LocationService to TODO comments *Added some constant on Constant.java

--- ProjectBLZ/app/src/main/java/edu/temple/projectblz/Constant.java
+++ ProjectBLZ/app/src/main/java/edu/temple/projectblz/Constant.java
@@ -1,4 +1,8 @@
 package edu.temple.projectblz;
 
 public class Constant {
+
+    public static final String Log_LocationUpdateInitialize = "The Location Update initialized";
+    public static final String LOG_LocationUpdateEnded = "The Location Service Ended";
+
 }
---
+++ ProjectBLZ/app/src/main/java/edu/temple/projectblz/LocationService.java
@@ -0,0 +1,86 @@
+package edu.temple.projectblz;
+
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+
+import java.io.Serializable;
+
+
+
+public class LocationService extends Service {
+    //Initialization of the location service based off TA's version
+
+    //Initialization of the Location Manager, Listener and Notification
+    private static final int FOREGROUND_SERVICE_ID = 1;
+    private LocationManager locationManager;
+    private LocationListener listener;
+    private Notification notification;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        listener = location -> {
+
+            //TODO Updating our features for our own listener
+
+        };
+
+        buildForegroundNotification();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            startForeground(FOREGROUND_SERVICE_ID, notification);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    10, 1, listener);
+            //TODO Initializing the distance based of 1 meter and 10 ms but would need to change if needed
+            Log.d(Constant.Log_LocationUpdateInitialize, "The foreground location service has sucessfully started: Location Update initialization is sucessful");
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+        //TODO implements onBind
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(Constant.LOG_LocationUpdateEnded, "Location Service is onDestroy and the Location Service is eliminated");
+        locationManager.removeUpdates(listener);
+        //Use Manager to remove the listener;
+    }
+
+    /**
+     * Construct a notification to the user to let them know what we're doing with this service
+     */
+    private void buildForegroundNotification() {
+        //TODO Setting our own buildforegroundNotification
+    }
+    
+
+}
