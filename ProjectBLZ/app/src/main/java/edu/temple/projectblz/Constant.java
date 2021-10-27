f38358e1941215cf168b2241ce73e6c75bd606c7
Ricky Zhou <59936830+ZhouRicky@users.noreply.github.com>
2021-10-26 22:44:53 -0400

    Implemented Runtime Permission Check
    - Expanded loginButton code
    - Refactored variable & view names to be more informative

--- ProjectBLZ/app/src/main/java/edu/temple/projectblz/Constant.java
+++ ProjectBLZ/app/src/main/java/edu/temple/projectblz/Constant.java
@@ -2,18 +2,17 @@ package edu.temple.projectblz;
 
 public class Constant {
 
-    //Constant for LocationService
-    public static final String Log_LocationUpdateInitialize = "The Location Update initialized";
-    public static final String LOG_LocationUpdateEnded = "The Location Service Ended";
+    // constants for LocationService
+    public static final String LOG_LOCATION = "Location";
 
-
-    //Constant for SharedPrefs
+    // constants for SharedPrefs
     public static final String SHARED_PREFS_NAME = "ProjectBLZ";
     public static final String SHARED_PREFS_DEFAULT_STRING = "None";
     public static final String USERNAME = "username";
+    public static final String IS_LOCATION_PERMISSION_GRANTED = "is_location_permission_granted";
 
-    //Constant for LoginActivity
-    public static final String InvalidInput = "Invalid Input";
-    public static final String InvalidInputMessage = null; //TODO Might use for an alertdialog
+    // constants for LoginActivity
+    public static final String INVALID_INPUT = "Please enter username and/or password";
+    public static final String INCORRECT_INFO = "Username and/or Password incorrect.";
 
 }
--- ProjectBLZ/app/src/main/java/edu/temple/projectblz/LoginActivity.java
+++ ProjectBLZ/app/src/main/java/edu/temple/projectblz/LoginActivity.java
@@ -1,51 +1,119 @@
 package edu.temple.projectblz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
-    TextView LoginTextView,QuestionTextView,SignUpTextView,Status;
-    Button LoginButton;
-    EditText UsernameEditText,PasswordEditText;
-    String Username,Password;
+
+    SharedPrefs sharedPrefs;
+
+    TextView statusTextView, signUpTextView;
+    EditText usernameEditText, passwordEditText;
+    Button loginButton;
+
+    boolean isLocationPermissionGranted;
+    String username, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ViewInitialization();
 
+        sharedPrefs = new SharedPrefs(this);
 
-        LoginButton.setOnClickListener(new View.OnClickListener() {
+        checkPermission();
+        viewInitialization();
+
+        // log in button functionality
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(UsernameEditText.getText()!=null&&PasswordEditText.getText()!=null){
-                    Username = UsernameEditText.getText().toString();
-                    Password = UsernameEditText.getText().toString();
-                    //TODO Implement php verifying credential
+                statusTextView.setText("");
+
+                if(usernameEditText.getText() != null && passwordEditText.getText() != null) {
+                    username = usernameEditText.getText().toString();
+                    password = passwordEditText.getText().toString();
+
+                    if(username.isEmpty() || password.isEmpty()) {
+                        statusTextView.setText(Constant.INCORRECT_INFO);
+                    } else {
+                        if(!isLocationPermissionGranted) {
+                            Toast.makeText(LoginActivity.this, "Please enable location permissions", Toast.LENGTH_SHORT).show();
+                        } else {
+                            login();
+                        }
+                    }
+                } else {
+                    statusTextView.setText(Constant.INVALID_INPUT);
                 }
-                else{
-                    Status.setText(Constant.InvalidInputMessage);
-                    //Might be better with alertDialog
-                    //InvalidInputMessage is defined null currenly in Constant.java
-                }
+            }
+        });
+
+        // sign up button functionality
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: open to new activity/fragment to allow user to create an account
             }
         });
     }
 
-    private void ViewInitialization(){
-        LoginTextView       = findViewById(R.id.Login);
-        QuestionTextView    = findViewById(R.id.Question);
-        SignUpTextView      = findViewById(R.id.SignUp);
-        UsernameEditText    = findViewById(R.id.Username);
-        PasswordEditText    = findViewById(R.id.Password);
-        LoginButton         = findViewById(R.id.LoginButton);
-        Status              = findViewById(R.id.Status);
+    // uses dexter library to check for permissions at runtime
+    private void checkPermission() {
+        Dexter.withContext(this).withPermissions(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                if(multiplePermissionsReport.areAllPermissionsGranted()) {
+                    isLocationPermissionGranted = true;
+                    sharedPrefs.setAccessLocationPermissionGranted(true);
+                }
+
+                if(multiplePermissionsReport.getDeniedPermissionResponses().size() > 0){
+                    Toast.makeText(LoginActivity.this, "All permissions are required to continue", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), "");
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    // initialize necessary views
+    private void viewInitialization() {
+        statusTextView = findViewById(R.id.statusTextView);
+        signUpTextView = findViewById(R.id.signUpTextView);
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+    }
+
+    // TODO: log in request; Implement php verifying credential
+    private void login() {
+
     }
 }
