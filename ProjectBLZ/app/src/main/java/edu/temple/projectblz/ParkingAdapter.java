834ae3481a4dc982f08c9411fd2a254cb6f894a8
Ricky Zhou <59936830+ZhouRicky@users.noreply.github.com>
2021-11-16 20:01:34 -0500

    Converted ListView to RecyclerView

--- ProjectBLZ/app/src/main/java/edu/temple/projectblz/ParkingAdapter.java
+++ ProjectBLZ/app/src/main/java/edu/temple/projectblz/ParkingAdapter.java
@@ -1,102 +1,86 @@
 package edu.temple.projectblz;
 
-import static android.content.Context.MODE_PRIVATE;
-
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class ParkingAdapter extends BaseAdapter {
-    private final String MESSAGE_KEY = "message";
+public class ParkingAdapter extends RecyclerView.Adapter<ParkingAdapter.MyViewHolder> {
+
     private Context context;
-    private ArrayList<String> title1;
-    SharedPreferences.Editor editor;
-    SharedPreferences prefs;
+    private ArrayList<String> itemsList;
 
-    public ParkingAdapter (Context context, ArrayList<String> title1) {
+    public ParkingAdapter(Context context, ArrayList<String> mList) {
         this.context = context;
-        this.title1 = title1;
-        title1.add("user1");
-        title1.add("user2");
-        title1.add("user3");
-        title1.add("user4");
+        itemsList = mList;
 
+        itemsList.add("user1");
+        itemsList.add("user2");
+        itemsList.add("user3");
+        itemsList.add("user4");
     }
 
-    @Override
-    public int getCount() {
-        return title1.size();
-    }
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView timeTextView;
+        private ImageButton deleteButton;
 
-    @Override
-    public Object getItem(int position) {
-        return title1.get(position);
-    }
+        public MyViewHolder(final View view) {
+            super(view);
 
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View row = inflater.inflate(R.layout.row_items, null, true);
-        TextView textView;
-        //TextView textView = row.findViewById(R.id.textView);
-        ImageView imageView = row.findViewById(R.id.imageView);
-        imageView.setColorFilter(Color.RED);
-        imageView.setMaxHeight(7);
-        //textView.setGravity(Gravity.CENTER);
-        // textView.setTextSize(20);
-        // TextView textView;
-        if (convertView instanceof TextView) {
-            textView = (TextView) convertView;
-        } else {
-            // textView = new TextView(context);
-            textView = row.findViewById(R.id.textView);
-            textView.setPadding(5,8,8,5);
-            textView.setGravity(Gravity.CENTER);
-            textView.setTextSize(20);
+            timeTextView = view.findViewById(R.id.timeTextView);
+            deleteButton = view.findViewById(R.id.deleteButton);
         }
-        final int pos = position;
-        textView.setText(getItem(position).toString());
-        prefs = context.getSharedPreferences(MESSAGE_KEY, MODE_PRIVATE);
-        editor = prefs.edit();
-        imageView.setOnClickListener(new View.OnClickListener() {
+    }
+
+    @NonNull
+    @Override
+    public ParkingAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View placeholderView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_items, parent, false);
+        return new MyViewHolder(placeholderView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ParkingAdapter.MyViewHolder holder, int position) {
+        int index = position;
+        String date = itemsList.get(position);
+        holder.timeTextView.setText(date);
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new AlertDialog.Builder(context)
                         .setIcon(android.R.drawable.ic_delete)
-                        .setTitle("Are you sure?")
-                        .setMessage("Do you want to delete this item?")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        .setTitle("Delete Parking Location")
+                        .setMessage("Do you want to delete this location?")
+                        .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                editor.remove(getItem(pos).toString());
-                                title1.remove(getItem(pos).toString());
+                                itemsList.remove(index);
                                 notifyDataSetChanged();
-                                editor.apply();
                             }
                         })
-                        .setNegativeButton("No", null)
+                        .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
                         .show();
             }
         });
-
-        return row;
     }
 
+    @Override
+    public int getItemCount() {
+        return itemsList.size();
+    }
 }
 
--- ProjectBLZ/app/src/main/java/edu/temple/projectblz/ParkingItemsActivity.java
+++ ProjectBLZ/app/src/main/java/edu/temple/projectblz/ParkingItemsActivity.java
@@ -1,83 +1,32 @@
 package edu.temple.projectblz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 public class ParkingItemsActivity extends AppCompatActivity {
 
-    private final String MESSAGE_KEY = "message";
-    ArrayList<String> title;
-    ListView listView;
-    BaseAdapter adapter;
-    SharedPreferences prefs;
-    SharedPreferences.Editor editor;
-    ImageView imageView;
-    Map<String, String> allEntries;
-    int pos;
-    int count = 0;
+    private ArrayList<String> itemsList;
+    private RecyclerView recyclerView;
+    ParkingAdapter parkingAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_items);
-        setTitle("BookmarksActivity");
-        listView = findViewById(R.id.listView);
-        imageView = findViewById(R.id.imageView);
-        prefs = getSharedPreferences(MESSAGE_KEY, MODE_PRIVATE);
-        editor = prefs.edit();
-        title = new ArrayList<>();
+        setTitle("Parking Location History");
 
-        addTitle();//add title(s) to list
-        adapter = new ParkingAdapter(this, title);
+        recyclerView = findViewById(R.id.parkingItemsRecyclerView);
+        itemsList = new ArrayList<>();
 
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent iIntent = getIntent();
-               // iIntent.putExtra(BrowserActivity.WORD, allEntries.get(parent.getItemAtPosition(position).toString()));
-                setResult(RESULT_OK, iIntent);
-                pos = position;
-                ParkingItemsActivity.this.finish();
-            }
-
-        });
-
-
-        //long click to delete item
-       /* listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(final AdapterView<?> parent, View view, final int position, long id) {
-                new AlertDialog.Builder(BookmarksActivity.this)
-                        .setIcon(android.R.drawable.ic_delete)
-                        .setTitle("Are you sure?")
-                        .setMessage("Do you want to delete this item?")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                editor.remove(parent.getItemAtPosition(position).toString());
-                                title.remove(parent.getItemAtPosition(position));
-                                editor.apply();
-                                adapter.notifyDataSetChanged();
-                            }
-                        })
-                        .setNegativeButton("No", null)
-                        .show();
-                return true;
-            }
-        });*/
+        setAdapter();
 
         findViewById(R.id.closeItemBtn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,18 +35,13 @@ public class ParkingItemsActivity extends AppCompatActivity {
                 finish();
             }
         });
-
     }
 
-    //get title
-    public void addTitle(){
-        count = prefs.getAll().size();
-        if(count>0) {
-            allEntries = (Map<String, String>) prefs.getAll();
-            for (Map.Entry<String, String> entry : allEntries.entrySet()) {
-                title.add(entry.getKey());
-            }
-        }
+    private void setAdapter() {
+        parkingAdapter = new ParkingAdapter(this, itemsList);
+        RecyclerView.LayoutManager layoutManager =new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(parkingAdapter);
     }
-
 }
