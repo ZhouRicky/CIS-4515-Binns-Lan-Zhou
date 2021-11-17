08dbf0102d440ee249d2512682654dec03d4bd77
Ricky Zhou <59936830+ZhouRicky@users.noreply.github.com>
2021-11-16 23:05:32 -0500

    Update Constant.java

--- ProjectBLZ/app/src/main/java/edu/temple/projectblz/Constant.java
+++ ProjectBLZ/app/src/main/java/edu/temple/projectblz/Constant.java
@@ -10,10 +10,8 @@ public class Constant {
 
     // constants for LocationService
     public static final String LOG_LOCATION = "Location";
-    public static final int FOREGROUND_SERVICE_ID = 1;
 
-
-    //location coordinates
+    // location coordinates
     public static final String LATITUDE = "Lat";
     public static final String LONGITUDE = "Lon";
 
@@ -29,36 +27,16 @@ public class Constant {
     // constants for LoginActivity
     public static final String INVALID_INPUT = "Please enter username and/or password.";
     public static final String INCORRECT_INFO = "Username and/or Password incorrect.";
-    public static final String LoginPhp = "http://192.168.1.78/login.php";
 
     // constants for SignUpActivity
     public static final String ENTER_ALL_INFO = "Please enter all information.";
     public static final String PW_DO_NOT_MATCH = "Passwords do not match.";
-    public static final String RegisterPhp = "http://192.168.1.78/register.php";
 
-    //Constant for MainActivity
-        //Constant for Tag
-        public static final String TAG = "Main Activity";
-
-        //Constant for Volley
-        public static final String SUCCESS_CODE = "success";
-        //Constant for RequestCode
-        public static final int RequestCode_FineLocation = 123;
-        public static final int RequestCode_WriteSetting = 234;
-        //Constant for brightness
-        public static final int Brightness_Zero = 0;
-        public static final int Brightness_Max = 255;
-        //Constant for URL
-        public static final String ParkPhp = "http://192.168.1.78/insertpark.php";
-        //Constant for Parking
-        //Modified Constant
-        public static final String ParkingLatitude = "park_lat";
-        public static final String ParkingLongitude = "park_lon";
-        public static final String ParkingDriverID= "driver_id";
-        //Constant for RequestCode_Permission_WriteSetting
-        public static final int RequestCode_Permission_WriteSetting = 100;
-
-
-
-
+    // Constant for MainActivity
+    public static final int RequestCode_WriteSetting = 234;
+    // Constant for brightness
+    public static final int Brightness_Zero = 0;
+    public static final int Brightness_Max = 255;
+    //Constant for RequestCode_Permission_WriteSetting
+    public static final int RequestCode_Permission_WriteSetting = 100;
 }
